5b16ae75dd725bb4351ddf7d3383aeb118ca8b17
Aziz <mu.aziz97@yahoo.com>
2022-03-10 00:31:16 +0700

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  const test = 'Hello World!';
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p className="my-6 text-3xl font-bold">{test}</p>
-      </header>
+      <Navbar />
     </div>
   );
 }
