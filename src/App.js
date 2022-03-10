e61bac3816456330c7d43317c57c742e503e442a
Aziz <mu.aziz97@yahoo.com>
2022-03-10 23:25:33 +0700

    feat: update navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+
+      <div className="container max-w-7xl my-6 mx-auto px-2 sm:px-6 lg:px-8">
+        <div>
+          <h1 className="font-bold text-2xl">Homepage</h1>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
